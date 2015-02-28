4825dfbb1779da9a62cdf2b54728d8d4273f83e6
Nikolay Lagutko <nikolay.lagutko@mail.com>
2015-02-28 10:43:58 +0300

    some test data

---
+++ src/main/java/org/gerzog/spock/spring/injectmock/InjectMocksExtension.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2015 Nikolay Lagutko <nikolay.lagutko@mail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gerzog.spock.spring.injectmock;
+
+import org.spockframework.runtime.extension.IGlobalExtension;
+import org.spockframework.runtime.model.SpecInfo;
+
+/**
+ * @author Nikolay Lagutko (nikolay.lagutko@mail.com)
+ *
+ */
+public class InjectMocksExtension implements IGlobalExtension {
+
+	private static final String[] DEFAULT_ANNOTATION_CLASSES = {
+			// java's @Resource
+			"javax.annotation.Resource",
+			// javax' @Inject
+			"javax.Inject",
+			// guice's @Inject
+			"com.google.Inject",
+			// spring's @Autowired
+			"org.springframework.beans.factory.annotation.Autowired",
+			// spring's @Required
+			"org.springframework.beans.factory.annotation.Required" };
+
+	@Override
+	public void visitSpec(final SpecInfo spec) {
+
+	}
+
+}
--- src/test/groovy/org/gerzog/spock/spring/injectmock/test/data/FieldInjection.groovy
+++ src/test/groovy/org/gerzog/spock/spring/injectmock/test/data/FieldInjection.groovy
@@ -13,21 +13,27 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.gerzog.spock.spring.injectmock;
+package org.gerzog.spock.spring.injectmock.test.data
 
-import org.spockframework.runtime.extension.IGlobalExtension;
-import org.spockframework.runtime.model.SpecInfo;
+import javax.annotation.Resource
+import javax.inject.Inject
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
 
 /**
  * @author Nikolay Lagutko (nikolay.lagutko@mail.com)
  *
  */
-public class InjectMockExtension implements IGlobalExtension {
+@Service
+class FieldInjection {
 
-	@Override
-	public void visitSpec(SpecInfo spec) {
-		// TODO Auto-generated method stub
+	@Autowired
+	def autowiredField
 
-	}
+	@Inject
+	def injectField
 
+	@Resource
+	def resourceField
 }
