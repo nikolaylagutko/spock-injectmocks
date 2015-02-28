22e76d3cd4d14fe124786814371e1941ae097488
Nikolay Lagutko <nikolay.lagutko@mail.com>
2015-02-28 10:02:07 +0300

    travis configuration

---
+++ src/main/java/org/gerzog/spock/spring/injectmock/InjectMockExtension.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright 2015 Nikolay Lagutko <nikolay.lagutko@mail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gerzog.spock.spring.injectmock;
+
+import org.spockframework.runtime.extension.IGlobalExtension;
+import org.spockframework.runtime.model.SpecInfo;
+
+/**
+ * @author Nikolay Lagutko (nikolay.lagutko@mail.com)
+ *
+ */
+public class InjectMockExtension implements IGlobalExtension {
+
+	@Override
+	public void visitSpec(SpecInfo spec) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
